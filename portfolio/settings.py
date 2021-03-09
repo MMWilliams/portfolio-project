2a51ba6e5f36de23bb14eb34a0a4919ae1eec92b
maureesewilliams <maureesewilliams@gmail.com>
2021-03-08 19:48:37 -0500

    chanded user to djangodb

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,10 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'postgres',
+        'USER': 'djangodb',#postgres
         'PASSWORD': 'Welcome1@',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5433'
     }
 }
 
